125673636f1237675f20def170c063cb02b67b9e
Patrick Best <patrickbest@patrickbest.com>
2013-11-27 17:21:22 -0600

    change server script name to descriptive

--- fmnm-server.js
+++ fmnm-server.js
@@ -1,6 +1,6 @@
 var static = require('node-static');
 // Create a node-static server instance to serve the './public' folder
-var file = new static.Server('./public', { cache: 7200, serverInfo: 'NOVARASERVE'});
+var file = new static.Server('./public', { cache: 259200, serverInfo: 'NOVARASERVE'});
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
     	file.serve(request, response);
